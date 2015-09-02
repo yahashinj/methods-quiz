53f48606db7b4d835872649fe28370293386d718
Naoki Yahashi <yahashinj@s.dcsdk12.org>
2015-09-02 09:57:22 -0600

    Added the code to add not in the words

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,9 +1,17 @@
 module MethodsQuiz
 	
 	# TODO - write has_teen?
-	
+	def has_teen?(age)
+		if age <= 19 && age >= 13
+			return true
+		else
+			return false
+		end
+	end
 	# TODO - write not_string
-	
+	def not_string(string)
+		return "not " + string 
+	end
 	# TODO - write icy_hot?
 	
 	# TODO - write closer_to
